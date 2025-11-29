f596ae6e1ce538b54ac04d9e2dd22f1d93830ddf
serrabruna <brunaibserra@hotmail.com>
2025-11-28 22:16:09 -0300

    inicio da página cadastro

--- Lectus/src/app/app.ts
+++ Lectus/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './core/header/header';
+import { Cadastro } from './features/cadastro/cadastro';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Cadastro],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
