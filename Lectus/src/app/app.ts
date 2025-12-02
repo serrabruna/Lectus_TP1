2ed8d551ae4e25fe503156ff058d1078da14a2f2
serrabruna <brunaibserra@hotmail.com>
2025-12-02 13:53:56 -0300

    desenvolvimento do componente footer

--- Lectus/src/app/app.ts
+++ Lectus/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './core/header/header';
 import { Cadastro } from './features/cadastro/cadastro';
 import { MenuLateral } from './core/menu-lateral/menu-lateral';
+import { Footer } from './core/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Cadastro, MenuLateral],
+  imports: [RouterOutlet, Header, Cadastro, MenuLateral, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
