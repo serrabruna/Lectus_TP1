c7a156473c5ee71bb45397103e1cfc82e4968947
serrabruna <brunaibserra@hotmail.com>
2025-12-03 14:54:56 -0300

    loggerService, CadastroService e desenvolvimento do formulário de cadastro

---
+++ Lectus/src/app/core/services/logger/logger.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggerService {
+  info (msg: string, extra?: unknown){
+    //enviar um endpoint posteriormente
+    console.info(msg, extra ?? '');
+  }
+
+  warn(msg: string, extra?: unknown){
+    console.warn(msg, extra ?? '');
+  }
+
+  error(msg: string, extra?: unknown){
+    console.error(msg, extra ?? '');
+  }
+}
