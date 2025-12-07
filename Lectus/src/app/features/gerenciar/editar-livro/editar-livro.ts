3acf211e1cad25e53335706835b0295f6dc7a8c5
serrabruna <brunaibserra@hotmail.com>
2025-12-06 23:55:33 -0300

    inicio de editar-livro.ts

--- Lectus/src/app/features/gerenciar/editar-livro/editar-livro.ts
+++ Lectus/src/app/features/gerenciar/editar-livro/editar-livro.ts
@@ -1,7 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, Signal, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { LivroService } from '../../catalogo/service/livro.service';
+import { CategoriaService } from '../../categorias/service/categoria.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Livro } from '../../../model/livro';
+import { Categoria } from '../../../model/categoria';
 
 @Component({
   selector: 'app-editar-livro',
@@ -10,5 +15,15 @@ import { RouterLink } from '@angular/router';
   styleUrl: './editar-livro.css',
 })
 export class EditarLivro {
+  private router = inject(Router);
+  private livroService = inject(LivroService);
+  private categoriaService = inject(CategoriaService);
+
+  livro = signal<Livro | null>(null);
+  categorias: Signal<Categoria[]> = toSignal(this.categoriaService.listar(), { initialValue: [] });
+
+  enviando = signal(false);
+  mensagem = signal('');
+
   
 }
